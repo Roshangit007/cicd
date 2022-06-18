68ff9696562d62503274d705c17ef6f2a79952ec
Roshan <63492990+Roshangit007@users.noreply.github.com>
2022-06-18 18:05:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V2!  US WHite House Hacked by me
-})
+  res.send('CICD App V2!  US WHite House Hacked by me')})
 
 app.get('/status', (req, res) => {
     res.send('ok')
