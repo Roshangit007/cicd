38589df84a373f1c2f1f8e175902711ef1e0fb87
Roshan <63492990+Roshangit007@users.noreply.github.com>
2022-06-20 15:18:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V2!  DEMO ECS')})
+  res.send('CICD App V2!  Ganu')})
 
 app.get('/status', (req, res) => {
     res.send('ok')
